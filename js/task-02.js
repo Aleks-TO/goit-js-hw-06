2c8452a25be202f635a625715b091cff1f2632fe
Aleks-TO <satov69@gmail.com>
2023-02-24 20:37:59 +0200

    2-task-complite

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+// отримуємо спочатку посилання на елемент дом інгрідієнти
+const ingredientsList = document.querySelector("#ingredients");
+
+// проходимось методом фор ич по всьому масиву
+ingredients.forEach(item => {
+	// створюємо елемент
+	const ingredientItem = document.createElement("li");
+	// додаэмо текстовий контент
+	ingredientItem.textContent = item;
+	// додаэмо клас
+	ingredientItem.classList.add("item");
+	// додаемо в дом
+	ingredientsList.append(ingredientItem);
+	// console.log(ingredientsList);
+});
