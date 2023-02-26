a3353ea82fd31405ecf999dc13e4357aca3e3e3d
Aleks-TO <satov69@gmail.com>
2023-02-26 09:08:05 +0200

    6-task-complite

---
+++ js/task-06.js
@@ -0,0 +1,20 @@
+// отримуэмо посилання
+const inputField = document.querySelector("#validation-input");
+
+// ставимо слухача на подію input
+inputField.addEventListener("blur", handleCheckInput);
+
+// Знаходимо значення атрибута data-length
+const dataLength = parseInt(inputField.getAttribute("data-length"));
+console.log(inputField.value.length);
+
+// пишемо функцію
+function handleCheckInput(e) {
+	if (inputField.value.length === dataLength) {
+		inputField.classList.add("valid");
+	} else {
+		inputField.classList.remove("valid");
+		inputField.classList.add("invalid");
+	}
+	// console.log(e);
+}
