b0edf278b22c1e3496b095e936d4c77b93f65d85
Aleks-TO <satov69@gmail.com>
2023-02-26 12:17:57 +0200

    8-task-complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,25 @@
 // отримуэмо посилання
-const form = document.querySelector(".login-form");
-console.log(form);
+const formEl = document.querySelector(".login-form");
+// console.log(formEl.elements);
+const emailInput = formEl.elements.email;
+const passInput = formEl.elements.password;
+const btnSubmit = formEl.lastElementChild;
 
 // ставимо слухача на подію submit
-form.addEventListener("submit", handleCheckForm);
+formEl.addEventListener("submit", handleCheckForm);
 
 // пишемо функцію
 function handleCheckForm(event) {
 	event.preventDefault(); // відключаємо перезавантаження
+
+	if (emailInput.value === "" && passInput.value === "") {
+		// emailInput.style.border = `1px solid red`;
+		return alert("ERROR!!!  Please fill oll fields");
+	}
+	const dataForm = {};
+	dataForm["email"] = emailInput.value;
+	dataForm["password"] = passInput.value;
+
+	console.log(dataForm);
+	formEl.reset();
 }
